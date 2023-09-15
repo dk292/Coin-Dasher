fa2598a1f223dd821b1432d5ff8b2ecd15735a5a
Htet Hlaing Win <htethlaingwin.bdm29@gmail.com>
2023-09-15 12:43:02 +0630

    Adding Main Scene and Modify Player Script

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -2,10 +2,15 @@ extends CharacterBody2D
 
 @export var speed: float = 500
 
+var direction: Vector2 = Vector2.ZERO
+var screensize: Vector2i = Vector2i(480, 720)
+
 func _process(_delta):
-	var direction = Input.get_vector("left", "right", "up", "down")
+	direction = Input.get_vector("left", "right", "up", "down")
 	velocity = direction * speed
 	move_and_slide()
+	position.x = clamp(position.x, 0, screensize.x)
+	position.y = clamp(position.y, 0, screensize.y)
 	
 	if(direction.length() > 0):
 		$AnimatedSprite2D.animation = "run"
@@ -14,3 +19,32 @@ func _process(_delta):
 		
 	if(velocity.x != 0):
 		$AnimatedSprite2D.flip_h = velocity.x < 0;
+		
+func start():
+	set_process(true)
+	position = screensize * 0.5
+	$AnimatedSprite2D.animation = "idle"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
